74671ea3143f190a8acbedeb774bf184463171b1
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-07-24 21:57:04 +0530

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import MenuContextProvider from './context/MenuContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/EverGreen">
     <MenuContextProvider>
       <App />
     </MenuContextProvider>
